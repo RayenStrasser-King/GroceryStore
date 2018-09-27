c4078a2a227fe96f1d79a3673b4a807001b2f6d3
RayenStrasser-King <rfstrasser-king@mail.csuchico.edu>
2018-09-26 17:38:53 -0700

    loops

--- main.cpp
+++ main.cpp
@@ -1,19 +1,41 @@
+//Author: Rayen Strasser-King
 #include<iostream>
 #include<string>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
+        string list[5]; //array of 5 strings
+        int numItems = 0;
+        char input;
+        string nm1;
 
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
 
-  return 0;
+
+        do{
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
+                if (input == 'A' || input == 'a')         
+                {
+                        {cout<<"What is the item?\n";
+                                cin>>nm1;
+                                if (numItems<5)
+                                list[numItems] = nm1;
+                                numItems++;
+                        }
+                }
+
+
+                if (numItems>5)
+                { cout<<"You'll need a bigger list!\n";}
+        }
+        while (input != 'q' && input != 'Q');
+
+
+        return 0;
 }
+
