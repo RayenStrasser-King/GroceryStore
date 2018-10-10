4d69b7ac27cf29d85b0bdd9ae0346e988fd7321d
RayenStrasser-King <rfstrasser-king@mail.csuchico.edu>
2018-10-10 16:53:25 -0700

    deletes

--- main.cpp
+++ main.cpp
@@ -32,8 +32,9 @@ int main()
                 }
                 if (input== 'd' || input== 'D' && list.size()>0)
                   {
-                    list.pop_back();
+                    //list.pop_back();
                     cout<<nm1<<" was deleted from the list.\n";
+                    list.pop_back();
                     //nm1 = list[list.size()-1];
                   }
           }
